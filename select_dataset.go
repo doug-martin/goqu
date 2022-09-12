339c3fe0bf60599f2f29d1ebad83355dee27a7dd
Roman Nikonov <code@nic11.xyz>
2022-09-12 20:16:30 +0200

    Fix description for Select

--- select_dataset.go
+++ select_dataset.go
@@ -198,7 +198,7 @@ func (sd *SelectDataset) WithRecursive(name string, subquery exp.Expression) *Se
 	return sd.copy(sd.clauses.CommonTablesAppend(exp.NewCommonTableExpression(true, name, subquery)))
 }
 
-// Adds columns to the SELECT clause. See examples
+// Replaces columns of the SELECT clause. Empty list resets the clause. See examples
 // You can pass in the following.
 //   string: Will automatically be turned into an identifier
 //   Dataset: Will use the SQL generated from that Dataset. If the dataset is aliased it will use that alias as the
@@ -214,7 +214,7 @@ func (sd *SelectDataset) Select(selects ...interface{}) *SelectDataset {
 	return sd.copy(sd.clauses.SetSelect(exp.NewColumnListExpression(selects...)))
 }
 
-// Adds columns to the SELECT DISTINCT clause. See examples
+// Replaces columns of the SELECT DISTINCT clause. Empty list resets the clause. See examples
 // You can pass in the following.
 //   string: Will automatically be turned into an identifier
 //   Dataset: Will use the SQL generated from that Dataset. If the dataset is aliased it will use that alias as the
